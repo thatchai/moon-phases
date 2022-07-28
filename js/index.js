85425b63be8d48b1815976c98706e17461149e2f
LAPTOP-N5BKRE2F\IT <a@a.com>
2022-07-28 11:53:47 +0700

    update ui

--- js/index.js
+++ js/index.js
@@ -1,83 +1,84 @@
 //
 const setMoonRotation = deg => {
-  document.querySelector('.divider').style.transform = `rotate3d(0, 1, 0, ${deg}deg)`
-
-  const hemispheres = document.querySelectorAll('.hemisphere')
-
-  if (deg < 180) {
-    // Left
-    hemispheres[0].classList.remove('dark')
-    hemispheres[0].classList.add('light')
-
-    // Right
-    hemispheres[1].classList.add('dark')
-    hemispheres[1].classList.remove('light')
-  } else {
-    // Left
-    hemispheres[0].classList.add('dark')
-    hemispheres[0].classList.remove('light')
-
-    // Right
-    hemispheres[1].classList.remove('dark')
-    hemispheres[1].classList.add('light')
-  }
-}
-
-//
-const onClickSearchBtn = () => {
-  let dateArr
-  let moonP
-  let selectDate
-  let angle
-  let waterValue
-
-  //
-  if (!dateInputEl.value) {
-    return
+    document.querySelector('.divider').style.transform = `rotate3d(0, 1, 0, ${deg}deg)`
+  
+    const hemispheres = document.querySelectorAll('.hemisphere')
+  
+    if (deg < 180) {
+      // Left
+      hemispheres[0].classList.remove('dark')
+      hemispheres[0].classList.add('light')
+  
+      // Right
+      hemispheres[1].classList.add('dark')
+      hemispheres[1].classList.remove('light')
+    } else {
+      // Left
+      hemispheres[0].classList.add('dark')
+      hemispheres[0].classList.remove('light')
+  
+      // Right
+      hemispheres[1].classList.remove('dark')
+      hemispheres[1].classList.add('light')
+    }
   }
   
-  //moon rotation
-  selectDate = new Date(dateInputEl.value)
-  angle = moonFuncGetMoonPhaseRotation(selectDate)
-  setMoonRotation(angle)
- 
-  //moon phase data
-  dateArr = dateSplit(dateInputEl.value)
-  moonP = moonFuncGetMoonPhase(dateArr[0], dateArr[1], dateArr[2])
-  setMoonPhaseName(moonFuncMoonPhaseData(moonP))
-
   //
-  waterValue = waterFuncWaterData(dateArr[0], dateArr[1], dateArr[2])
-  setWaterValue(waterValue)
-
+  const onClickSearchBtn = () => {
+    let dateArr
+    let moonP
+    let selectDate
+    let angle
+    let waterValue
+  
+    //
+    if (!dateInputEl.value) {
+      return
+    }
+    
+    //moon rotation
+    selectDate = new Date(dateInputEl.value)
+    angle = moonFuncGetMoonPhaseRotation(selectDate)
+    setMoonRotation(angle)
+   
+    //moon phase data
+    dateArr = dateSplit(dateInputEl.value)
+    moonP = moonFuncGetMoonPhase(dateArr[0], dateArr[1], dateArr[2])
+    setMoonPhaseName(moonFuncMoonPhaseData(moonP))
+  
+    //
+    waterValue = waterFuncWaterData(dateArr[0], dateArr[1], dateArr[2])
+    setWaterValue(waterValue)
+  
+    //
+    setWaterHeightPercent(waterFuncWaterValuePercent(waterValue))
+  }
+  
+  const dateSplit = (dateStr) => {
+    return dateStr.split('-').map(Number)
+  }
+  
+  const setMoonPhaseName = phaseName => {
+    moonPhaseNameEl.innerHTML = phaseName
+  }
+  
+  const setWaterValue = waterValue => {
+    waterValueEl.innerHTML = waterValue
+  }
+  
+  const setWaterHeightPercent = percent => {
+    hWaterEl.style.height = percent + '%'
+  }
+  
+  //-- init --
+  let moonPhaseNameEl = document.querySelector('.moon-phase-name')
+  let waterValueEl = document.querySelector('.water-value')
+  let dateInputEl = document.querySelector('.date-input')
+  let searchBtnEl = document.querySelector('.search-btn')
+  let hWaterEl = document.querySelector('.h-water')
+  
   //
-  setWaterHeightPercent(waterFuncWaterValuePercent(waterValue))
-}
-
-const dateSplit = (dateStr) => {
-  return dateStr.split('-').map(Number)
-}
-
-const setMoonPhaseName = phaseName => {
-  moonPhaseNameEl.innerHTML = phaseName
-}
-
-const setWaterValue = waterValue => {
-  waterValueEl.innerHTML = waterValue
-}
-
-const setWaterHeightPercent = percent => {
-  hWaterEl.style.height = percent + '%'
-}
-
-//-- init --
-let moonPhaseNameEl = document.querySelector('.moon-phase-name')
-let waterValueEl = document.querySelector('.water-value')
-let dateInputEl = document.querySelector('.date-input')
-let searchBtnEl = document.querySelector('.search-btn')
-let hWaterEl = document.querySelector('.h-water')
-
-//
-searchBtnEl.addEventListener('click', onClickSearchBtn)
-
- 
+  searchBtnEl.addEventListener('click', onClickSearchBtn)
+  
+   
+  
--- js/moon-func.js
+++ js/moon-func.js
@@ -8,71 +8,71 @@
 //ref js:
 //  https://gist.github.com/endel/dfe6bb2fbe679781948c
 const moonFuncGetMoonPhase = (year, month, day) => {
-  var c = e = jd = b = 0;
-
-  if (month < 3) {
-      year--;
-      month += 12;
+    var c = e = jd = b = 0;
+  
+    if (month < 3) {
+        year--;
+        month += 12;
+    }
+  
+    ++month;
+  
+    c = 365.25 * year;
+  
+    e = 30.6 * month;
+  
+    jd = c + e + day - 694039.09; //jd is total days elapsed
+    
+    jd /= 29.5305882; //divide by the moon cycle
+    
+    b = parseInt(jd); //int(jd) -> b, take integer part of jd
+  
+    jd -= b; //subtract integer part to leave fractional part of original jd
+    
+    b = Math.round(jd * 8); //scale fraction from 0-8 and round
+     
+    if (b >= 8 ) {
+        b = 0; //0 and 8 are the same so turn 8 into 0
+    }
+  
+    // 0 => New Moon
+    // 1 => Waxing Crescent Moon
+    // 2 => Quarter Moon
+    // 3 => Waxing Gibbous Moon
+    // 4 => Full Moon
+    // 5 => Waning Gibbous Moon
+    // 6 => Last Quarter Moon
+    // 7 => Waning Crescent Moon
+    
+    return b;
   }
-
-  ++month;
-
-  c = 365.25 * year;
-
-  e = 30.6 * month;
-
-  jd = c + e + day - 694039.09; //jd is total days elapsed
   
-  jd /= 29.5305882; //divide by the moon cycle
+  //ref js:
+  //  https://dev.to/thormeier/use-your-i-moon-gination-lets-build-a-moon-phase-visualizer-with-css-and-js-aih
+  const moonFuncGetMoonPhaseRotation = date => {
+    const cycleLength = 29.5 // days
   
-  b = parseInt(jd); //int(jd) -> b, take integer part of jd
-
-  jd -= b; //subtract integer part to leave fractional part of original jd
+    const knownNewMoon = new Date('2022-03-02 18:34:00')
+    const secondsSinceKnownNewMoon = (date - knownNewMoon) / 1000
+    const daysSinceKnownNewMoon = secondsSinceKnownNewMoon / 60 / 60 / 24
+    const currentMoonPhasePercentage = (daysSinceKnownNewMoon % cycleLength) / cycleLength
   
-  b = Math.round(jd * 8); //scale fraction from 0-8 and round
-   
-  if (b >= 8 ) {
-      b = 0; //0 and 8 are the same so turn 8 into 0
+    return 360 - Math.floor(currentMoonPhasePercentage * 360)
   }
-
-  // 0 => New Moon
-  // 1 => Waxing Crescent Moon
-  // 2 => Quarter Moon
-  // 3 => Waxing Gibbous Moon
-  // 4 => Full Moon
-  // 5 => Waning Gibbous Moon
-  // 6 => Last Quarter Moon
-  // 7 => Waning Crescent Moon
   
-  return b;
-}
-
-//ref js:
-//  https://dev.to/thormeier/use-your-i-moon-gination-lets-build-a-moon-phase-visualizer-with-css-and-js-aih
-const moonFuncGetMoonPhaseRotation = date => {
-  const cycleLength = 29.5 // days
-
-  const knownNewMoon = new Date('2022-03-02 18:34:00')
-  const secondsSinceKnownNewMoon = (date - knownNewMoon) / 1000
-  const daysSinceKnownNewMoon = secondsSinceKnownNewMoon / 60 / 60 / 24
-  const currentMoonPhasePercentage = (daysSinceKnownNewMoon % cycleLength) / cycleLength
-
-  return 360 - Math.floor(currentMoonPhasePercentage * 360)
-}
-
-//
-const moonFuncMoonPhaseData = (phase) => {
-  let data = {
-    0: 'New Moon',
-    1: 'Waxing Crescent Moon',
-    2: 'Quarter Moon',
-    3: 'Waxing Gibbous Moon',
-    4: 'Full Moon',
-    5: 'Waning Gibbous Moon',
-    6: 'Last Quarter Moon',
-    7: 'Waning Crescent Moon'
-  }
-
   //
-  return data[phase]
-}
+  const moonFuncMoonPhaseData = (phase) => {
+    let data = {
+      0: 'New Moon',
+      1: 'Waxing Crescent Moon',
+      2: 'Quarter Moon',
+      3: 'Waxing Gibbous Moon',
+      4: 'Full Moon',
+      5: 'Waning Gibbous Moon',
+      6: 'Last Quarter Moon',
+      7: 'Waning Crescent Moon'
+    }
+  
+    //
+    return data[phase]
+  }
